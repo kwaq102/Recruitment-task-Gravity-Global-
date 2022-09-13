c59f0fafb0ea62c311ceda545b5b1537fc7fe2bc
Kamil Gajewski <kwaq102@gmail.com>
2022-09-13 10:15:28 +0200

    add counter at localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,28 @@
 const body = document.querySelector('body')
 const btn = document.querySelector('.btn');
+const btnReset = document.querySelector('.btn-reset')
 const popup = document.querySelector('.popup');
 const close = document.querySelector('.close');
+const counterElement = document.querySelector('.counter');
+
+let counter = Number(localStorage.getItem('counter'));
 
 const showPopup = () => {
     popup.classList.add('show');
+    counter++;
+    localStorage.setItem('counter', counter.toString())
+    counterElement.textContent = counter;
 }
 
 const hidePopup = () => {
     popup.classList.remove('show');
+}
+
+const resetCounter = () => {
+    console.log('ok');
+    // localStorage.setItem('counter', '0');
+    localStorage.removeItem('counter');
+    counterElement.textContent = '0'
 }
 
 btn.addEventListener('click', showPopup);
@@ -29,3 +43,4 @@ popup.addEventListener('click', (e) => {
 })
 
 close.addEventListener('click', hidePopup);
+btnReset.addEventListener('click', resetCounter);
